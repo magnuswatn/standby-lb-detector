e96edef953da133f358fa42ef015ff558011c0d2
Magnus Watn <magnus@watn.no>
2019-07-31 09:25:53 +0200

    Style updates
    Use ES6 syntax and use prettier for formatting

--- background.js
+++ background.js
@@ -1,21 +1,18 @@
-function sendMessageToContentScript(responseDetails) {
-    chrome.tabs.sendMessage(responseDetails.tabId, {operation: "checkStatus"});
-}
+const sendMessageToContentScript = responseDetails => {
+  chrome.tabs.sendMessage(responseDetails.tabId, { operation: "checkStatus" });
+};
 
-chrome.webRequest.onCompleted.addListener(
-    sendMessageToContentScript,
-    {urls: ["https://*/xui/update/configuration/alert/statusmenu/coloradvisory*"] }
-);
-
-chrome.pageAction.onClicked.addListener(function(tab) {
-    chrome.tabs.sendMessage(tab.id, {operation: "stopUpdating"});
-    chrome.pageAction.hide(tab.id);
+chrome.webRequest.onCompleted.addListener(sendMessageToContentScript, {
+  urls: ["https://*/xui/update/configuration/alert/statusmenu/coloradvisory*"]
 });
 
-chrome.runtime.onMessage.addListener(
-    function(request, sender, sendResponse) {
-        if (request.operation == "activateButton") {
-            chrome.pageAction.show(sender.tab.id);
-        }
-    }
-);
+chrome.pageAction.onClicked.addListener(tab => {
+  chrome.tabs.sendMessage(tab.id, { operation: "stopUpdating" });
+  chrome.pageAction.hide(tab.id);
+});
+
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  if (request.operation === "activateButton") {
+    chrome.pageAction.show(sender.tab.id);
+  }
+});
